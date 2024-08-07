5f1d5b6fd9379253d6440dfcef7e0178ca9862ca
MaralTach <yoloten.maral@gmail.com>
2024-08-07 21:12:52 +0300

    up

--- src/dummydata.js
+++ src/dummydata.js
@@ -2,19 +2,19 @@ export const homeAbout = [
   {
     id: 1,
     cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
-    title: "Online Courses",
+    title: " Residence Appointment ",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
   },
   {
     id: 1,
     cover: "https://img.icons8.com/ios/80/000000/diploma.png",
-    title: "Earn A Certificates",
+    title: "Invitation to Universities ",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
   },
   {
     id: 1,
     cover: "https://img.icons8.com/ios/80/000000/athlete.png",
-    title: "Learn with Expert",
+    title: "Health Insurance",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
   },
 ]
